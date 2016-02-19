2f1b72d80a902f14a48c65e55188d9c4daac3572
Max Plomer <maxplomer@gmail.com>
2016-02-18 20:46:30 -0500

    add back slogan for mobile

--- my_javascript.js
+++ my_javascript.js
@@ -20,9 +20,12 @@ $( document ).ready(function() {
   if (location.pathname.split('/').slice(-1)[0] == 'staging.html' && $(window).width() > 580) {
     setTimeout(function(){ $( "#top-right-menu" ).fadeTo( "slow", 1 );
     }, 2100);  
+    setTimeout(function(){ $( "#sub-logo-mobile" ).fadeTo( "slow", 1 );
+    }, 2100);  
   } else {
     //$( "#top-right-menu" ).hide();
     $( "#top-right-menu" ).css('opacity', 1);
+    $( "#sub-logo-mobile" ).css('opacity', 1);
   };
 
 
